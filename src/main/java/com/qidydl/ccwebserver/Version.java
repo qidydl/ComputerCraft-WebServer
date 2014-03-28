b83697e58f066bcabac22b88a4f7da7a253c689f
David Osolkowski <qidydl@gmail.com>
2014-03-27 21:33:35 -0400

    Fix versioning scheme
    It's still not ideal but at least it should sort of work.

--- src/main/java/com/qidydl/ccwebserver/Version.java
+++ src/main/java/com/qidydl/ccwebserver/Version.java
@@ -4,10 +4,9 @@ import java.util.Properties;
 
 public class Version {
     private static String major;
+    private static String medium;
     private static String minor;
     private static String rev;
-    private static String build;
-    @SuppressWarnings("unused")
     private static String mcversion;
 
     static void init(Properties properties)
@@ -15,15 +14,15 @@ public class Version {
         if (properties != null)
         {
             major = properties.getProperty("ccwebserver.build.major.number");
+            medium = properties.getProperty("ccwebserver.build.medium.number");
             minor = properties.getProperty("ccwebserver.build.minor.number");
             rev = properties.getProperty("ccwebserver.build.revision.number");
-            build = properties.getProperty("ccwebserver.build.number");
             mcversion = properties.getProperty("ccwebserver.build.mcversion");
         }
     }
 
     public static String fullVersionString()
     {
-        return String.format("%s.%s.%s build %s", major, minor, rev, build);
+        return String.format("%s.%s.%s rev %s", major, medium, minor, rev);
     }
 }
