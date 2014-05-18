09d2e4f04cb6b1be9662bf6f377546ca0c18ba5d
David Osolkowski <qidydl@gmail.com>
2014-05-18 00:11:53 -0400

    Attempting to fix dependencies
    Changed around some versioning, I think Forge is happier now but still
    doesn't recognize us as a child mod of Computer Craft

--- src/main/java/com/qidydl/ccwebserver/Version.java
+++ src/main/java/com/qidydl/ccwebserver/Version.java
@@ -3,26 +3,26 @@ package com.qidydl.ccwebserver;
 import java.util.Properties;
 
 public class Version {
-    private static String major;
-    private static String medium;
-    private static String minor;
-    private static String rev;
-    private static String mcversion;
+	private static String major;
+	private static String medium;
+	private static String minor;
+	private static String rev;
+	private static String mcversion;
 
-    static void init(Properties properties)
-    {
-        if (properties != null)
-        {
-            major = properties.getProperty("ccwebserver.build.major.number");
-            medium = properties.getProperty("ccwebserver.build.medium.number");
-            minor = properties.getProperty("ccwebserver.build.minor.number");
-            rev = properties.getProperty("ccwebserver.build.revision.number");
-            mcversion = properties.getProperty("ccwebserver.build.mcversion");
-        }
-    }
+	static void init(Properties properties)
+	{
+		if (properties != null)
+		{
+			major = properties.getProperty("qidydlCCWebServer.build.major.number");
+			medium = properties.getProperty("qidydlCCWebServer.build.medium.number");
+			minor = properties.getProperty("qidydlCCWebServer.build.minor.number");
+			rev = properties.getProperty("qidydlCCWebServer.build.revision.number");
+			mcversion = properties.getProperty("qidydlCCWebServer.build.mcversion");
+		}
+	}
 
-    public static String fullVersionString()
-    {
-        return String.format("%s.%s.%s rev %s", major, medium, minor, rev);
-    }
+	public static String fullVersionString()
+	{
+		return String.format("%s.%s.%s rev %s", major, medium, minor, rev);
+	}
 }
